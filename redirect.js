0cd96a41b4ca33f41d6fb6343e09ec40a46f0704
Fraser Reilly <26841319+fraserreilly@users.noreply.github.com>
2021-10-25 09:01:28 -0700

    Update redirect.js

--- redirect.js
+++ redirect.js
@@ -1,27 +1,27 @@
 // ==UserScript==
 // @name         Youtube Subscriptions Redirect
-// @namespace    https://github.com/spacegIider
+// @namespace    https://github.com/fraserreilly
 // @version      1.0
 // @license      MIT
 // @description  Shows all subscribed videos in subscription feed.
-// @author       Spaceglider
+// @author       fraserreilly
 // @match        *://www.youtube.com/feed/subscriptions
 // @grant        none
-// @homepageURL  https://github.com/spacegIider/Youtube-Subscriptions-Redirect
-// @supportURL   https://github.com/spacegIider/Youtube-Subscriptions-Redirect/issues
-// @downloadURL  https://raw.githubusercontent.com/spacegIider/Youtube-Subscriptions-Redirect/redirect.js
-// @updateURL    https://raw.githubusercontent.com/spacegIider/Youtube-Subscriptions-Redirect/redirect.js
+// @homepageURL  https://github.com/fraserreilly/Youtube-Subscriptions-Redirect
+// @supportURL   https://github.com/fraserreilly/Youtube-Subscriptions-Redirect/issues
+// @downloadURL  https://raw.githubusercontent.com/fraserreilly/Youtube-Subscriptions-Redirect/redirect.js
+// @updateURL    https://raw.githubusercontent.com/fraserreilly/Youtube-Subscriptions-Redirect/redirect.js
 // ==/UserScript==
 
-var BadDumbYoutubeURL = window.location.pathname;
+var bYoutubeURL = window.location.pathname;
 
-if ( ! /\?flow=1$/.test (BadDumbYoutubeURL) ) {
-    var ActuallySeeAllVideosURL = window.location.protocol + "//"
+if ( ! /\?flow=1$/.test (badYoutubeURL) ) {
+    var DisplayAllVideosURL = window.location.protocol + "//"
                                 + window.location.host
-                                + BadDumbYoutubeURL + "?flow=1"
+                                + badYoutubeURL + "?flow=1"
                                 +window.location.search
                                 +window.location.hash
                                 ;
 
-    window.location.replace (ActuallySeeAllVideosURL);
+    window.location.replace (DisplayAllVideosURL);
 }
